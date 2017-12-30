97a86e6988bc3ab6292c4c9bcc7532436cc9b4c4
github username <github email address>
2017-12-30 12:57:24 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    @comment = Comment.create(comment_params)
+    redirect_to post_path(@comment.post)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :user_id, :post_id, user_attributes: [:username, :email] )
   end
 end
